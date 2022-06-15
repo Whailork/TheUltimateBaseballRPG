7cee938cf44ac136155536ef3b83f78230c9babf
Memoh <CraftMan3512@gmail.com>
2022-06-15 11:14:52 -0400

    BICHETTE MOVE

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    public float speed = 1f;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        rb = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        float x = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(x*speed, rb.velocity.y);
+
     }
 }
